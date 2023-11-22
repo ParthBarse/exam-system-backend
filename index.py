5fd479c85860c91cc049e5a07bf35bcb94eae2ff
Parth Barse <parthbarse72@gmail.com>
2023-11-23 01:01:07 +0530

    update

---
+++ index.py
@@ -0,0 +1,64 @@
+from flask import Flask
+from flask_login import login_user
+from flask import request, session
+from pymongo import MongoClient
+from flask import Flask, request, jsonify
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_cors import CORS
+import random
+import json
+import pandas as pd
+from email.mime.text import MIMEText
+import smtplib
+
+app = Flask(__name__)
+CORS(app)
+
+client = MongoClient(
+    'mongodb+srv://bnbdevs:feLC7m4jiT9zrmHh@cluster0.fjnp4qu.mongodb.net/?retryWrites=true&w=majority')
+app.config['MONGO_URI'] = 'mongodb+srv://bnbdevs:feLC7m4jiT9zrmHh@cluster0.fjnp4qu.mongodb.net/?retryWrites=true&w=majority'
+app.config['SECRET_KEY'] = 'a6d217d048fdcd227661b755'
+db = client['mcf_db']
+# db2 = client['BnB_all_customers']
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = "ic2023wallet@gmail.com"
+app.config['MAIL_PASSWORD'] = "irbnexpguzgxwdgx"
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/home')
+def home():
+    return 'home page'
+
+
+# ------------------------------------------------------------------------------------------------------------
+
+
+@app.route("/getAllStudents", methods=["GET"])
+def getAllStudents():
+    users = db["students_db"]
+    ans = users.find({},{'_id':0})
+    return ans
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
+
+
+
+
+
