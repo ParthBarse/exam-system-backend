25217443d0a31e0d6996f8f0ac5de80958a70b11
Parth Barse <parthbarse72@gmail.com>
2024-03-31 11:07:39 +0530

    update

--- index.py
+++ index.py
@@ -1509,20 +1509,20 @@ def update_student():
                 print("BATCH ID ---> ",student_cp['batch_id'], " ------ ", value)
                 if student_cp['batch_id'] != value:
                     update_sid = True
-                    generateSacTableAndRecountRegStudents(prev_batch_id)
-                    sync_SacTableFrom_Student(prev_batch_id)
+                    # generateSacTableAndRecountRegStudents(prev_batch_id)
+                    # sync_SacTableFrom_Student(prev_batch_id)
             if key == "camp_id":
                 print("CAMP ID ---> ",student_cp['camp_id'], " ------ ", value)
                 if student_cp['camp_id'] != value:
                     update_sid = True
-                    generateSacTableAndRecountRegStudents(prev_batch_id)
-                    sync_SacTableFrom_Student(prev_batch_id)
+                    # generateSacTableAndRecountRegStudents(prev_batch_id)
+                    # sync_SacTableFrom_Student(prev_batch_id)
             if key == "company":
                 print("COMPANY ---> ",student_cp['company'], " ------ ", value)
                 if student_cp['company'] != value:
                     update_sid = True
-                    generateSacTableAndRecountRegStudents(prev_batch_id)
-                    sync_SacTableFrom_Student(prev_batch_id)
+                    # generateSacTableAndRecountRegStudents(prev_batch_id)
+                    # sync_SacTableFrom_Student(prev_batch_id)
             if key != 'sid':
                 student[key] = value
 
